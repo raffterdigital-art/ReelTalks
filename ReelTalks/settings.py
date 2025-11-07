5264bcf6bd20431c560d5676c83bd7fc4168be02
subbu <raffterdigital@gmail.com>
2025-11-08 00:58:17 +0530

    changess

--- ReelTalks/settings.py
+++ ReelTalks/settings.py
@@ -130,12 +130,20 @@ CKEDITOR_5_CONFIGS = {
         },
     }
 }
-cloudinary.config(
-    cloud_name="dhpfmobxq",       # Your Cloudinary Cloud Name
-    api_key="679143996711284",    # Your API Key
-    api_secret="yhICb9C3dLMdYcBUxI7yTDH_SF0",  # Your API Secret
-    secure=True
-)
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dhpfmobxq',
+    'API_KEY': '679143996711284',
+    'API_SECRET': 'yhICb9C3dLMdYcBUxI7yTDH_SF0',
+    'SECURE': True,
+    'UPLOAD_OPTIONS': {
+        'use_filename': True,
+        'unique_filename': False,
+        'overwrite': True,   # optional: overwrite same name if re-uploaded
+    }
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 # Django storage settings
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
