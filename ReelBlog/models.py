d2656cb2ce33a2d011beb2041c61c2192e4f9fbb
subbu <raffterdigital@gmail.com>
2025-11-08 16:03:04 +0530

    mergecomss

--- ReelBlog/models.py
+++ ReelBlog/models.py
@@ -16,12 +16,11 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    images = CloudinaryField('image', folder='cd3168f6508975cb897a9ac8db6285cf35', blank=True, null=True)
     content = CKEditor5Field('Text', config_name='default')
     created_at = models.DateTimeField(auto_now_add=True)
     trending = models.BooleanField(default=False)
-    images = CloudinaryField('image', folder='cd3168f6508975cb897a9ac8db6285cf35', blank=True, null=True)
-
+   
     class Meta:
         ordering = ['-created_at']
 
